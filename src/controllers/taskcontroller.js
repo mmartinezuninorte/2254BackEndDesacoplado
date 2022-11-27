4e19ebdc789b58f68266b591840d1427e5691d70
mmartinezuninorte <mmartinezuninorte@gmail.com>
2022-11-27 09:53:30 -0500

    Finalizacion sesion  14

--- src/controllers/taskcontroller.js
+++ src/controllers/taskcontroller.js
@@ -19,23 +19,54 @@ export const newTask = async (req, res)=>{
 
 //Manejo de error con estado 500 en caso de crash
 export const findAllTask = async (req, res)=>{
-    const tareas = await Task.find()
-    res.json(tareas)
+    try {
+        const tareas = await Task.find()
+        res.json(tareas)  
+    } catch (error) {
+        res.status(500).json({
+            message:  "Opps, algo fallo al consultar el listado de tareas"
+        })
+    }
 }
 
 export const findOneTask = async (req, res)=>{
-    const tarea = await Task.findById(req.params.id)
-    res.json(tarea)
+    const id = req.params.id
+    try {
+        const tarea = await Task.findById(id)
+        if(!tarea) return res.status(404).json({message: `la tarea con id: ${id} no existe`})
+        res.json(tarea)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message || `Error al buscar la tarea con id: ${id}`
+        })}
 }
 
+// Validacion para la no existencia de una tarea al momento de eliminarla
 export const deleteTask = async (req, res)=>{
     const id = req.params.id
-    await Task.findByIdAndDelete(id)
-    res.json({message: `La tarea con id: ${id}, ha sido eliminada satisfactoriamente`})
+    try {
+        await Task.findByIdAndDelete(id)
+        res.json({message: `La tarea con id: ${id}, ha sido eliminada satisfactoriamente`})
+    } catch (error) {
+        res.status(500).json({
+            message: error.message || `Opps, ocurrio un error al intentar eliminar la tarea con id: ${id}`
+        })
+    }
 }
 
 export const updateTask = async (req, res)=>{
     const { id } = req.params
-    await Task.findByIdAndUpdate(id, req.body)
-    res.json({message: `La tarea con id ${id}, ha sido actualizada!`})
+    
+    if (!req.body.title){
+      return res.status(400).send({message: "El campo titulo ESTA VACIO!!"})  
+    }
+
+    try {
+        await Task.findByIdAndUpdate(id, req.body)
+        res.json({message: `La tarea con id ${id}, ha sido actualizada!`})
+    } catch (error) {
+        res.status(500).json({
+            message: error.message || `Error al actualizar la tarea con id ${id}`
+        })
+    }
 }
