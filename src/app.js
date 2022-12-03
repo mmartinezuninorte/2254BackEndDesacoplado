8c05a8cd6e057721c7e7d534d979384e5298e8ce
mmartinezuninorte <mmartinezuninorte@gmail.com>
2022-12-03 10:51:36 -0500

    ExpressMonitor

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import taskRoutes from './routes/task.router'
 import authRoutes from './routes/auth.router'
+import expressStatusMonitor from 'express-status-monitor'
 
 const app = express()
 
 app.set('port', 3003)
 
 app.use(express.json())
+app.use(expressStatusMonitor())
 
 app.get('/', (req, res)=>{
     res.json({message: "Bienvenido a mi backEnd! Comunicate mediante peticiones a mi API-REST"})
